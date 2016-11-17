97f4e8136d60d829f3c1c0e4fffc8d9a67ed2cee
Inna <inna.piddema@sannacode.com>
2016-11-17 23:07:25 +0200

    JUnit test_ class_work

---
+++ MathFunc.java
@@ -0,0 +1,21 @@
+package LessonNew;
+
+public class MathFunc {
+
+    public static int multiply(int a, int b){return a * b;}
+
+}
+/* package LessonNew-название пакета;
+import org.junit.Assert;
+        import org.junit.Test;
+
+public class MathFuncTest - название класса {
+    @Test // JUnit framework annotation
+    public void multiplyTest(){
+        int a = 3;
+        int b = 2;
+        int expRes = 6;
+        Assert.assertEquals(expRes, MathFunc.multiply(a, b));
+    }
+} - создается в другом классе, чтобы запустить тест ( другой класс наз. MathFuncTest.java)
+*/
